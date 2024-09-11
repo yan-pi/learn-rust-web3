876471003c11579cda3f86c956f85a68956fa4be
Yan Fernandes <102800044+yan-pi@users.noreply.github.com>
2024-09-11 00:00:18 -0300

    lesson-5: Refactor balance transfer logic and add unit tests

--- src/balances.rs
+++ src/balances.rs
@@ -18,22 +18,59 @@ impl Pallet {
     pub fn get_balance(&self, account: String) -> u128 {
         *self.balances.get(&account).unwrap_or(&0)
     }
+
+    pub fn transfer(
+        &mut self, 
+        caller: String,
+        to: String,
+        amount: u128,
+    ) -> Result<(), &'static str> {
+        let caller_balance = self.get_balance(caller.clone());
+        let to_balance = self.get_balance(to.clone());
+
+        let new_caller_balance = caller_balance.checked_sub(amount).ok_or("Insufficient balance")?;
+        let new_to_balance = to_balance.checked_add(amount).ok_or("Overflow")?;
+
+        self.balances.insert(caller, new_caller_balance);
+        self.balances.insert(to, new_to_balance);
+
+        Ok(())
+    }
 }
 
-#[test]
-fn init_balances() {
-    let mut balances = Pallet::new();
-    assert_eq!(balances.get_balance("yan".to_string()), 0);
-    balances.set_balance("yan".to_string(), 10);
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    assert_eq!(balances.get_balance("yan".to_string()), 10);
-    assert_eq!(balances.get_balance("bubu".to_string()), 0);
-}
-#[test]
-fn update_balance() {
-    let mut balances = Pallet::new();
-    balances.set_balance("yan".to_string(), 10);
-    balances.set_balance("yan".to_string(), 20);
+    #[test]
+    fn init_balances() {
+        let mut balances = Pallet::new();
+        assert_eq!(balances.get_balance("yan".to_string()), 0);
+        balances.set_balance("yan".to_string(), 10);
 
-    assert_eq!(balances.get_balance("yan".to_string()), 20);
+        assert_eq!(balances.get_balance("yan".to_string()), 10);
+        assert_eq!(balances.get_balance("bubu".to_string()), 0);
+    }
+
+    #[test]
+    fn update_balance() {
+        let mut balances = Pallet::new();
+        balances.set_balance("yan".to_string(), 10);
+        balances.set_balance("yan".to_string(), 20);
+
+        assert_eq!(balances.get_balance("yan".to_string()), 20);
+    }
+
+    #[test]
+    fn transfer_balance() {
+        let mut balances = Pallet::new();
+        balances.set_balance("Yan".to_string(), 20);
+
+        assert_eq!(
+            balances.transfer("Yan".to_string(), "Bruna".to_string(), 10),
+            Ok(())
+        );
+        assert_eq!(balances.get_balance("Yan".to_string()), 10);
+        assert_eq!(balances.get_balance("Bruna".to_string()), 10);
+    }
 }
